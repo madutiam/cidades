c714cf2a4e500650a184dfc610f3767a6ecadc91
Madutiam <conta.para.qualquer.coisa2005@gmail.com>
2022-03-04 22:10:51 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("São Vicente");
+Console.WriteLine("Santos");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruibe");
